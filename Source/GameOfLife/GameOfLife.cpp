7f4def49a5d78bee326954ccbf6a44dbce24edde
Hopson97 <mhopson@hotmail.co.uk>
2018-06-09 08:31:58 +0100

    Add canway's game of life

---
+++ Source/GameOfLife/GameOfLife.cpp
@@ -0,0 +1,14 @@
+#include "GameOfLife.h"
+
+GameOfLife::GameOfLife(const Config & config)
+    : CellularAutomaton(config) 
+{
+}
+
+void GameOfLife::input()
+{
+}
+
+void GameOfLife::update()
+{
+}
---
+++ Source/GameOfLife/GameOfLife.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#pragma once
+
+#include <vector>
+#include "../CellularAutomaton.h"
+
+
+class GameOfLife : public CellularAutomaton
+{
+    enum Cell
+    {
+        On,
+        Off
+    };
+
+public:
+    GameOfLife(const Config& config);
+
+    void input() override;
+    void update() override;
+
+private:
+    std::vector<Cell> m_cells;
+};
